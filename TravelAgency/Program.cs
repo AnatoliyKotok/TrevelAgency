31ba9401b8e4ed5854e00657bccce95fe3e2daf6
AnatoliyKotok <anatolijkotok@gmail.com>
2022-01-17 09:16:04 +0200

    firstcommit

---
+++ TravelAgency/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "client-app";
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseReactDevelopmentServer(npmScript:"start");
+    }
+});
+
+app.Run();
